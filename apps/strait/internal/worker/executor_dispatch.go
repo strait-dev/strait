e3d59f95272be6e4b5a8dc33a9679aae2e52c4ac
Leonardo Maldonado <leonardomso11@gmail.com>
2026-06-03 23:10:24 +0200

    refactor(worker): isolate job clone policy

--- apps/strait/internal/worker/executor_dispatch.go
+++ apps/strait/internal/worker/executor_dispatch.go
@@ -9,7 +9,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"maps"
 	"net/http"
 	"net/http/httptrace"
 	"strconv"
@@ -425,44 +424,6 @@ func (e *Executor) resolveJobForRun(ctx context.Context, run *domain.JobRun) (*d
 		return e.jobVersionCache.Load(ctx, jobVersionKey{JobID: run.JobID, Version: run.JobVersion}, loadVersion)
 	}
 	return loadVersion(ctx, jobVersionKey{JobID: run.JobID, Version: run.JobVersion})
-}
-
-func cloneJob(job *domain.Job) *domain.Job {
-	if job == nil {
-		return nil
-	}
-	cloned := *job
-	if job.Tags != nil {
-		cloned.Tags = maps.Clone(job.Tags)
-	}
-	if job.DefaultRunMetadata != nil {
-		cloned.DefaultRunMetadata = maps.Clone(job.DefaultRunMetadata)
-	}
-	if job.RetryDelaysSecs != nil {
-		cloned.RetryDelaysSecs = append([]int(nil), job.RetryDelaysSecs...)
-	}
-	if job.RateLimitKeys != nil {
-		cloned.RateLimitKeys = append([]domain.RateLimitKey(nil), job.RateLimitKeys...)
-	}
-	if job.PreferredRegions != nil {
-		cloned.PreferredRegions = append([]string(nil), job.PreferredRegions...)
-	}
-	if job.AllowedTools != nil {
-		cloned.AllowedTools = append([]string(nil), job.AllowedTools...)
-	}
-	if job.BlockedTools != nil {
-		cloned.BlockedTools = append([]string(nil), job.BlockedTools...)
-	}
-	if job.ResultSchema != nil {
-		cloned.ResultSchema = append(json.RawMessage(nil), job.ResultSchema...)
-	}
-	if job.OnCompletePayloadMapping != nil {
-		cloned.OnCompletePayloadMapping = append(json.RawMessage(nil), job.OnCompletePayloadMapping...)
-	}
-	if job.OnFailurePayloadMapping != nil {
-		cloned.OnFailurePayloadMapping = append(json.RawMessage(nil), job.OnFailurePayloadMapping...)
-	}
-	return &cloned
 }
 
 func (e *Executor) execute(ctx context.Context, run *domain.JobRun) {
